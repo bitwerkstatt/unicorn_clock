2a537d0155e32ca5f9283fc697a6400b724218ab
Thomas Manthey <mail@thomas-manthey.de>
2023-07-15 14:46:01 +0200

    Setting the time with X button only.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ clock = machine.RTC()
 
 handling_a_ts = time.ticks_ms()
 handling_b_ts = time.ticks_ms()
+handling_x_ts = time.ticks_ms()
 
 mode = MODE_RUNCLOCK
 hours_to_set = 0
@@ -64,12 +65,12 @@ def render_time(hours, minutes, seconds):
     # Seconds blinker
     for i in range(14):
         if seconds == 255: # Hack for settime
-            draw_lamp(1+i,3,1,1,0,255,0)
+            draw_lamp(1+i,3,1,1,0,0,255)
         else:
             if seconds % 2:
                 draw_lamp(1+i,3,1,1,136,34,17)
             else:
-                draw_lamp(1+i,3,1,1,20,20,0)
+                draw_lamp(1+i,3,1,1,20,20,20)
 
 
 def set_time(hours, minutes):
@@ -78,19 +79,23 @@ def set_time(hours, minutes):
 mode = MODE_RUNCLOCK
 
 while True:
-    if unicorn.is_pressed(unicorn.BUTTON_X):
-        mode = MODE_SETTIME
-        hours_to_set = clock.datetime()[4]
-        minutes_to_set = clock.datetime()[5]
-        
-    if unicorn.is_pressed(unicorn.BUTTON_Y):
-        clock.datetime((2023,7,15,0,hours_to_set, minutes_to_set,0,0))
-        mode = MODE_RUNCLOCK
-        
+    current_tick = time.ticks_ms()
+    handling_button_a = unicorn.is_pressed(unicorn.BUTTON_A) and not (current_tick - handling_a_ts) < BUTTON_IDLE_TIME 
+    handling_button_b = unicorn.is_pressed(unicorn.BUTTON_B) and not (current_tick - handling_b_ts) < BUTTON_IDLE_TIME
+    handling_button_x = unicorn.is_pressed(unicorn.BUTTON_X) and not (current_tick - handling_x_ts) < BUTTON_IDLE_TIME
+    
+    if handling_button_x:
+        if mode == MODE_SETTIME:
+            clock.datetime((2023,7,15,0,hours_to_set, minutes_to_set,0,0))
+            mode = MODE_RUNCLOCK
+        else:
+            hours_to_set = clock.datetime()[4]
+            minutes_to_set = clock.datetime()[5]
+            mode = MODE_SETTIME
+        handling_x_ts = current_tick
+              
     if mode == MODE_SETTIME:
-        current_tick = time.ticks_ms()
-        handling_button_a = unicorn.is_pressed(unicorn.BUTTON_A) and not (current_tick - handling_a_ts) < BUTTON_IDLE_TIME 
-        handling_button_b = unicorn.is_pressed(unicorn.BUTTON_B) and not (current_tick - handling_b_ts) < BUTTON_IDLE_TIME 
+                
         if handling_button_a:
             hours_to_set = (hours_to_set+1)%24
             handling_a_ts = current_tick 
@@ -99,7 +104,7 @@ while True:
             minutes_to_set = (minutes_to_set+1)%60
             handling_b_ts = current_tick
             
-        render_time(hours_to_set, minutes_to_set, 0)
+        render_time(hours_to_set, minutes_to_set, 255)
         
     else:
         current_time = clock.datetime()
